78c53b4c20ce17da1bb669a376ace0ca5c0e4a49
Joreg Reyes <reyesjorge48@gmail.com>
2021-03-27 13:45:11 -0400

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,23 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { Pages404Component } from './pages/pages404/pages404.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
-
+import { Pages404Component } from './pages404/pages404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    Pages404Component,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    Pages404Component, 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
+    AuthModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
